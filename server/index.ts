1cca1042e329520d2fec042d4ac7e38a2355b408
“Zane <zlh4@duke.edu>
2023-12-27 20:03:31 -0600

    Add params and state control for login

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,10 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { generateRandomString } from "./utils/helperFunctions";
 
 dotenv.config({ path: "../config.env" });
+
+const querystring = require('querystring');
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
@@ -14,8 +17,23 @@ app.get("/api", (req: Request, res: Response) => {
   res.json({ users: ["Tabitha", "is", "fart"] });
 });
 
+const stateKey = 'spotify_auth_state';
+
 app.get("/login", (req: Request, res: Response) => {
-  res.redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`);
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  const scope = 'user-read-private user-read-email';
+
+  const queryParams: string = querystring.stringify({
+    client_id: CLIENT_ID,
+    response_type: 'code',
+    redirect_uri: REDIRECT_URI,
+    state: state,
+    scope: scope,
+  });
+
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
 app.listen(port, () => {
