1779e070cbf0fd7f212bb232696f59fc22d2bb74
“Zane <zlh4@duke.edu>
2023-12-30 14:41:38 -0600

    Work on database connection file

--- server/db.ts
+++ server/db.ts
@@ -1,9 +1,40 @@
-const mysql = require("mysql2");
+const dotenv = require("dotenv");
+dotenv.config({ path: "../config.env" });
 
-mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'monthify_db'
+const mysql = require("mysql2");
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
 }).promise();
 
+async function getUsers() {
+    try {
+        const [rows] = await pool.query("SELECT * FROM users");
+        console.log(rows);
+        return rows;
+    } catch (error) {
+        console.error("Error querying the database:", error);
+    } finally {
+        pool.end();
+    }
+}
+
+async function getUser(id: number) {
+    try {
+        const [rows] = await pool.query(`
+        SELECT *
+        FROM users
+        WHERE id = ?
+        `, [id]);
+        console.log(rows[0]);
+        return rows[0];
+    } catch (error) {
+        console.error("Error querying the database:", error);
+    } finally {
+        pool.end();
+    }
+}
+
+getUser(1);
