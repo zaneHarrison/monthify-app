d0de19367e7c1dfccdd59ca035f18f7a7c4129ef
“Zane <zlh4@duke.edu>
2024-01-19 22:22:10 -0500

    Create function to retrieve current month from database

--- server/db.ts
+++ server/db.ts
@@ -1,154 +1,160 @@
-import dotenv from 'dotenv'
-dotenv.config({ path: '../config.env' })
+import dotenv from "dotenv";
+dotenv.config({ path: "../config.env" });
 
-import mysql, { RowDataPacket } from 'mysql2'
+import mysql, { RowDataPacket } from "mysql2";
 const pool = mysql
-    .createPool({
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-    })
-    .promise()
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
 
 // USERS TABLE
 
 // Add a new user
 export async function createUser(
-    spotify_display_name: string,
-    spotify_user_id: string,
-    refresh_token: string
+  spotify_display_name: string,
+  spotify_user_id: string,
+  refresh_token: string,
 ) {
-    try {
-        await pool.query(
-            `
+  try {
+    await pool.query(
+      `
         INSERT INTO users (spotify_display_name, spotify_user_id, refresh_token)
         VALUES (?, ?, ?)
         `,
-            [spotify_display_name, spotify_user_id, refresh_token]
-        )
-        console.log(
-            `User ${spotify_display_name} successfully added to database.`
-        )
-    } catch (error) {
-        console.error('Error inserting user into database:', error)
-    }
+      [spotify_display_name, spotify_user_id, refresh_token],
+    );
+    console.log(`User ${spotify_display_name} successfully added to database.`);
+  } catch (error) {
+    console.error("Error inserting user into database:", error);
+  }
 }
 
 // Retrieve a list of all users
 export async function getUsers() {
-    try {
-        const [rows] = await pool.query('SELECT * FROM users')
-        return rows as RowDataPacket[]
-    } catch (error) {
-        console.error('Error getting users from database:', error)
-        return []
-    }
+  try {
+    const [rows] = await pool.query("SELECT * FROM users");
+    return rows as RowDataPacket[];
+  } catch (error) {
+    console.error("Error getting users from database:", error);
+    return [];
+  }
 }
 
 // Retrieve a particular user from their id
 export async function getUserById(spotify_user_id: string) {
-    try {
-        const [rows] = await pool.query<RowDataPacket[]>(
-            `
+  try {
+    const [rows] = await pool.query<RowDataPacket[]>(
+      `
         SELECT *
         FROM users
         WHERE spotify_user_id = ?
         `,
-            [spotify_user_id]
-        )
-        return rows[0]
-    } catch (error) {
-        console.error('Error getting user from database:', error)
-    }
+      [spotify_user_id],
+    );
+    return rows[0];
+  } catch (error) {
+    console.error("Error getting user from database:", error);
+  }
 }
 
 // Delete a particular user from their id
 export async function deleteUser(spotify_user_id: string) {
-    try {
-        await pool.query(
-            `
+  try {
+    await pool.query(
+      `
         DELETE FROM users
         WHERE spotify_user_id = ?
         `,
-            [spotify_user_id]
-        )
-        console.log(
-            `User with username ${spotify_user_id} deleted successfully.`
-        )
-    } catch (error) {
-        console.error(
-            `Error deleting user with username ${spotify_user_id} from database:`,
-            error
-        )
-    }
+      [spotify_user_id],
+    );
+    console.log(`User with username ${spotify_user_id} deleted successfully.`);
+  } catch (error) {
+    console.error(
+      `Error deleting user with username ${spotify_user_id} from database:`,
+      error,
+    );
+  }
 }
 
 // Update a particular user's monthly_playlist_id value
 export async function updateUsersMonthlyPlaylistId(
-    spotify_user_id: string,
-    monthly_playlist_id: string
+  spotify_user_id: string,
+  monthly_playlist_id: string,
 ) {
-    try {
-        await pool.query(
-            `
+  try {
+    await pool.query(
+      `
         UPDATE users
         SET monthly_playlist_id = ?
         WHERE spotify_user_id = ?;
         `,
-            [monthly_playlist_id, spotify_user_id]
-        )
-        console.log(
-            `Successfully updated monthly_playlist_id value for user with username ${spotify_user_id}.`
-        )
-    } catch (error) {
-        console.error(
-            `Error updating monthly_playlist_id value for user with username ${spotify_user_id}:`,
-            error
-        )
-    }
+      [monthly_playlist_id, spotify_user_id],
+    );
+    console.log(
+      `Successfully updated monthly_playlist_id value for user with username ${spotify_user_id}.`,
+    );
+  } catch (error) {
+    console.error(
+      `Error updating monthly_playlist_id value for user with username ${spotify_user_id}:`,
+      error,
+    );
+  }
 }
 
 // Update a particular user's monthly_playlist_id value
 export async function updateMonthify30Id(
-    spotify_user_id: string,
-    monthify_30_id: string
+  spotify_user_id: string,
+  monthify_30_id: string,
 ) {
-    try {
-        await pool.query(
-            `
+  try {
+    await pool.query(
+      `
         UPDATE users
         SET monthify_30_id = ?
         WHERE spotify_user_id = ?;
         `,
-            [monthify_30_id, spotify_user_id]
-        )
-        console.log(
-            `Successfully updated monthify_30_id value for user with username ${spotify_user_id}.`
-        )
-    } catch (error) {
-        console.error(
-            `Error updating monthify_30_id value for user with username ${spotify_user_id}:`,
-            error
-        )
-    }
+      [monthify_30_id, spotify_user_id],
+    );
+    console.log(
+      `Successfully updated monthify_30_id value for user with username ${spotify_user_id}.`,
+    );
+  } catch (error) {
+    console.error(
+      `Error updating monthify_30_id value for user with username ${spotify_user_id}:`,
+      error,
+    );
+  }
 }
 
 // MONTH TABLE
 
 // Update the current month
-export async function updateMonth(current_month: number) {
-    try {
-        await pool.query(
-            `
+export async function updateCurrentMonth(current_month: number) {
+  try {
+    await pool.query(
+      `
         UPDATE month SET current_month = ?
         `,
-            [current_month]
-        )
-        console.log(
-            `Current month value updated to ${current_month} in database.`
-        )
-    } catch (error) {
-        console.error('Error updating current month value in database:', error)
-    }
+      [current_month],
+    );
+    console.log(`Current month value updated to ${current_month} in database.`);
+  } catch (error) {
+    console.error("Error updating current month value in database:", error);
+  }
+}
+
+// Get the value of the current month
+export async function getCurrentMonth() {
+  try {
+    const [rows] = await pool.query<RowDataPacket[]>(
+      "SELECT current_month FROM month",
+    );
+    return rows[0].current_month;
+  } catch (error) {
+    console.error("Error getting value of current month from database:", error);
+  }
 }
--- server/index.ts
+++ server/index.ts
@@ -1,76 +1,76 @@
-import express, { Express } from 'express'
-import dotenv from 'dotenv'
-import querystring from 'querystring'
-import axios from 'axios'
-import { AxiosError, AxiosResponse } from 'axios'
-import cookieParser from 'cookie-parser'
-import { getUsers } from './db.js'
-import { ToadScheduler, SimpleIntervalJob, Task } from 'toad-scheduler'
-import { getTracksFromPlaylist } from './utils/playlistLogic.js'
-import { RowDataPacket } from 'mysql2'
-import { createServerRoutes } from './routes/index.js'
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import querystring from "querystring";
+import axios from "axios";
+import { AxiosError, AxiosResponse } from "axios";
+import cookieParser from "cookie-parser";
+import { getCurrentMonth, getUsers } from "./db.js";
+import { ToadScheduler, SimpleIntervalJob, Task } from "toad-scheduler";
+import { getPlaylists, getTracksFromPlaylist } from "./utils/playlistLogic.js";
+import { RowDataPacket } from "mysql2";
+import { createServerRoutes } from "./routes/index.js";
 
 // Enable the use of environment variables
-dotenv.config({ path: '../config.env' })
+dotenv.config({ path: "../config.env" });
 
 // Access environment variables stored in .env file
-const CLIENT_ID = process.env.CLIENT_ID
-const CLIENT_SECRET = process.env.CLIENT_SECRET
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 // Create app, assign backend port
-const app: Express = express()
-const port = parseInt(process.env.SERVER_PORT || '', 10)
+const app: Express = express();
+const port = parseInt(process.env.SERVER_PORT || "", 10);
 
 // Add cookieParser middleware to application
-app.use(cookieParser())
+app.use(cookieParser());
 
-createServerRoutes(app)
+createServerRoutes(app);
 
 // Bind application to port
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
-})
+  console.log(`Server started on port ${port}`);
+});
 
 // CODE FOR SCHEDULED TASK
 
 // Scheduler for task
-const scheduler = new ToadScheduler()
+const scheduler = new ToadScheduler();
 
 // Define scheduled task to run
-const task = new Task('test-task', async () => {
-    console.log('TASK STARTING')
-    // Get all users from database
-    getUsers().then((response: RowDataPacket[]) => {
-        response.forEach((user) => {
-            const spotify_id = user.spotify_user_id
-            const refresh_token = user.refresh_token
-            // Use user's refresh token to get access token
-            axios({
-                method: 'post',
-                url: 'https://accounts.spotify.com/api/token',
-                data: querystring.stringify({
-                    grant_type: 'refresh_token',
-                    refresh_token: refresh_token,
-                }),
-                headers: {
-                    'content-type': 'application/x-www-form-urlencoded',
-                    Authorization: `Basic ${Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
-                },
-            })
-                .then((response: AxiosResponse) => {
-                    const access_token = response.data.access_token
-                    //getPlaylists(spotify_id, access_token);
-                    getTracksFromPlaylist(
-                        access_token,
-                        '0bYGcx0QW3w2RKYxdCF1AL'
-                    )
-                })
-                .catch((error: AxiosError) => {
-                    console.log(error)
-                })
+const task = new Task("test-task", async () => {
+  console.log("TASK STARTING");
+  // Get all users from database
+  getUsers().then((response: RowDataPacket[]) => {
+    response.forEach((user) => {
+      const spotify_id = user.spotify_user_id;
+      const refresh_token = user.refresh_token;
+      // Use user's refresh token to get access token
+      axios({
+        method: "post",
+        url: "https://accounts.spotify.com/api/token",
+        data: querystring.stringify({
+          grant_type: "refresh_token",
+          refresh_token: refresh_token,
+        }),
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          Authorization: `Basic ${Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64")}`,
+        },
+      })
+        .then((response: AxiosResponse) => {
+          const access_token = response.data.access_token;
+          //getPlaylists(spotify_id, access_token);
+          // getTracksFromPlaylist(
+          //     access_token,
+          //     '0bYGcx0QW3w2RKYxdCF1AL'
+          // )
         })
-    })
-})
+        .catch((error: AxiosError) => {
+          console.log(error);
+        });
+    });
+  });
+});
 
-const job = new SimpleIntervalJob({ seconds: 5 }, task)
-scheduler.addSimpleIntervalJob(job)
+const job = new SimpleIntervalJob({ seconds: 5 }, task);
+scheduler.addSimpleIntervalJob(job);
